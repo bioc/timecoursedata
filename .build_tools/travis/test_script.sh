ba3d4c1828c81b169c4438b8272b71d139b7112f
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-06-22 16:48:37 +0200

    Let's start the data package timecourse data

---
+++ .build_tools/travis/test_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+
+# License: 3-clause BSD
+
+set -e
+
+
+run_tests() {
+    # This runs simple "normal" + bioconductor checks on the package
+    make check
+    make test
+    make doc
+}
+
+# Start by installing
+make install
+make install-extra
+
+# Now get data and run the tests and build the manuscript
+run_tests
---
+++ Makefile
@@ -0,0 +1,28 @@
+RSCRIPT ?= Rscript
+
+
+all: doc-doxygen check build install test
+
+build:
+	$(RSCRIPT) .scripts/build_package.R
+
+install: doc-doxygen
+	$(RSCRIPT) .scripts/install_package.R
+
+install-extra:
+	$(RSCRIPT) .scripts/install_extra_dependencies.R
+
+check:
+	$(RSCRIPT) .scripts/check_package.R
+	R CMD BiocCheck
+
+doc-doxygen:
+	$(RSCRIPT) .scripts/build_documentation.R
+
+doc-html: install
+	$(RSCRIPT) .scripts/build_documentation_html.R
+
+doc: doc-html
+
+test:
+	$(RSCRIPT) .scripts/test_package.R
