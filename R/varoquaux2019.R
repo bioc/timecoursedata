af02461cac950acd13372b55b226643a788aa492
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-07-27 11:46:46 +0200

    MAINT using a data_env to avoid polluting the namespace
    - FIXed some documentation issue.
    - Merged the function to load leaf and root dataset.

--- R/varoquaux2019.R
+++ R/varoquaux2019.R
@@ -22,16 +22,15 @@
 #'   Any comparison between the two datasets should be traded with care.
 #' @details \code{data} is a data frame with 34,211 rows corresponding to
 #'   sorghum genes with
-#'   (\href{https://phytozome.jgi.doe.gov/pz/portal.html#!info?alias=Org_Sbicolor}{Phytozome v3.0} gene ids). Root has 198 columns and leaf has 197 columns,
+#'   (\href{https://phytozome.jgi.doe.gov/pz/portal.html#!info?alias=Org_Sbicolor}{Phytozome
+#'   v3.0} gene ids). Root has 198 columns and leaf has 197 columns,
 #'   corresponding to samples. The root and leaf samples are from the same
 #'   plants, and have ids in the form of "0622162L05" or "0622162R05", with "L"
 #'   corresponding to the leaf and "R" corresponding to the root sample. Some
 #'   samples were removed for quality control reasons, resulting in slightly
 #'   different numbers of samples between the leaf and root.
-#' @details The data provided is normalized data, where the leaf and root have
-#'   been normalized separately. While we have kept the datasets in the same
-#'   format, a simple merge of the two datasets could be problematic due to the
-#'   separate normalization.
+#' @details The data provided is the raw data. Note that leafs and roots were
+#'   sequenced as two different batches.
 #' @details \code{meta} is a data frame with rows corresponding to samples and
 #'   and columns with meta data. These columns are the same across leaf and
 #'   root, allowing for easy reuse of code across the datasets:
@@ -117,24 +116,3 @@
 #' dataRoot = attr(varoquaux2019root, "data")
 #' metaRoot = attr(varoquaux2019root, "meta")
 "varoquaux2019leaf"
-
-## I recommend removing these variables:
-  # \item{Plate1"         XXX
-  # \item{Week2D"  XXX
-  # \item{Group"     XXX
-  # \item{Timepoint"    XXX
-  # \item{badSamples"            XXX
-  # \item{questionableSamples"      XXX
-  # \item{Mislabeled" XXX
-  # \item{Genotype.Sample.type"}XXX
-  # \item{CompleteTreatment}XXX
-  # \item{Collected.By}XXX
-  # \item{Notes}XXX
-  # \item{FloweringJulianDate.1"XXX
-  # \item{fileUsed"         XXX
-  # \item{processed_as"        XXX
-  # \item{sampleName"      XXXX
-  # \item{conditionNumber" ????
-  # \item{BarcodeJGI"XXX
-  # \item{isCorrectedBarcode"  XXX
-  # \item{SampleId"XXX
